4308ecee35dcc867349198fcece929d36e649b9d
Jesper Jaxing <jaxing@student.chalmers.se>
2017-09-21 12:04:41 +0200

    now this works :D

--- assignment1/task3_b_new.m
+++ assignment1/task3_b_new.m
@@ -36,43 +36,48 @@ biase_out = rand*2 - 1;
 
 energy_train = zeros(1,10^3);
 energy_validation = zeros(1,10^3);
-iteration_range = 1:10^3;
+iteration_range = 1:10^6;
 
 train_error = class_error(training_target, training_input, weights_out, weights_hid, biase_out, biase_hid)
 val_error = class_error(validation_target, validation_input, weights_out, weights_hid, biase_out, biase_hid)
 tic
-
+l=1;
 for iteration = iteration_range
-    tic
     i = mod(iteration, length(training_target));
     if i == 0
         i = length(training_target);
     end
     
-    predict_hid = predict(training_input(i,:).', weights_hid, biase_hid);
+    predict_hid = predict(training_input(i,:), weights_hid, biase_hid);
     predict_out = predict(predict_hid, weights_out, biase_out);
     
-    l = energy_function(training_target, training_input, weights_out, weights_hid, biase_out, biase_hid);
-    energy_train(i) = l;
-    energy_validation(i) = energy_function(validation_target, validation_input, weights_out, weights_hid, biase_out, biase_hid);
+    if mod(iteration, 1000) == 0
+        energy_train(l) = energy_function(training_target, training_input, weights_out, weights_hid, biase_out, biase_hid);
+        energy_validation(l) = energy_function(validation_target, validation_input, weights_out, weights_hid, biase_out, biase_hid);
+        l = l + 1;
+    end
     
-    weights_out = update_weights_out(training_target(i,:), predict_out, weights_out, predict_hid, biase_out)
-    weights_hid = update_weights_hid(training_target(i,:), predict_out, predict_hid, weights_out, weights_hid, training_input(i,:), biase_out, biase_hid)
-    one_it = toc;
+    weights_out = weights_out + update_weights_out(training_target(i,:), predict_out, weights_out, predict_hid, biase_out);
+    biase_out = biase_out + update_biase_out(training_target(i,:), predict_out, weights_out, predict_hid, biase_out);
+    weights_hid = weights_hid + update_weights_hid(training_target(i,:), predict_out, predict_hid, weights_out, weights_hid, training_input(i,:), biase_out, biase_hid);
+    biase_hid = biase_hid + update_biase_hid(training_target(i,:), predict_out, predict_hid, weights_out, weights_hid, training_input(i,:), biase_out, biase_hid);
 end
 toc
 train_error = class_error(training_target, training_input, weights_out, weights_hid, biase_out, biase_hid)
 val_error = class_error(validation_target, validation_input, weights_out, weights_hid, biase_out, biase_hid)
 
 hold on
-plot(iteration_range, energy_train);
-plot(iteration_range, energy_validation)
+length(energy_train)
+plot(1:10^3, energy_train);
+plot(1:10^3, energy_validation);
+xlabel('1000 iterations')
+ylabel('energy')
 
 function H = energy_function(target, input, weights, weights_hid, biase, biase_hid)
     summa = 0;
     p=length(target);
     for i=1:p
-        predict_hid = predict(input(i,:).', weights_hid, biase_hid);
+        predict_hid = predict(input(i,:), weights_hid, biase_hid);
         predict_out = predict(predict_hid, weights, biase);
         summa = summa + (target(i) - predict_out).^2;
     end
@@ -86,7 +91,7 @@ function c = class_error(targets, inputs, weights, weights_hid, biase, biase_hid
     
     for i=1:p
         
-        predict_hid = predict(inputs(i,:).', weights_hid, biase_hid);
+        predict_hid = predict(inputs(i,:), weights_hid, biase_hid);
         predict_out = predict(predict_hid, weights, biase);
         
         summa = summa + abs(targets(i) - sign(predict_out));
@@ -95,27 +100,50 @@ function c = class_error(targets, inputs, weights, weights_hid, biase, biase_hid
 end
 
 function f = predict(input, weights, biase)
-    tmp = (weights * input);
-    f = activation(tmp - biase);
+    f = activation(weights * input.' - biase).';
 end
 
 function W = update_weights_hid(targets, predicted, predicted_hid, weights_out, weights_hid, input, biase_out, biase_hid)
     learning_rate = 0.02;
     beta = 1/2;
     
-    delta_error_out = sech(beta * (weights_out * predicted_hid - biase_out))^2* beta * predicted_hid * (targets - predicted);
-    delta_error = input.' * sech(beta * (input * weights_hid.' - biase_hid.'))^2 * beta * (weights_out.' .* delta_error_out);
+    g_prime_out = sech(beta * (weights_out * predicted_hid.' - biase_out)).^2 * beta * predicted_hid;
+    g_prime_hid = sech(beta * (weights_hid * input.' - biase_hid)).^2 * beta;
     
-    W = (learning_rate * weights_hid.' .* delta_error).';
+    delta_error_out = (targets - predicted) * g_prime_out;
+    delta_error = (delta_error_out .* weights_out).'  .* g_prime_hid;
+    
+    W = learning_rate * delta_error * input;
+end
+
+function b = update_biase_hid(targets, predicted, predicted_hid, weights_out, weights_hid, input, biase_out, biase_hid)
+    learning_rate = 0.02;
+    beta = 1/2;
+    
+    g_prime_out = sech(beta * (weights_out * predicted_hid.' - biase_out)).^2 * beta;
+    g_prime_hid = sech(beta * (weights_hid * input.' - biase_hid)).^2 * beta;
+    
+    delta_error_out = (targets - predicted) * -g_prime_out;
+    delta_error_hid = delta_error_out .* weights_out.' .* -g_prime_hid;
+    
+    b = learning_rate * delta_error_hid;
 end
 
 function W = update_weights_out(targets, predicted, weights_out, input, biase)
     learning_rate = 0.02;
     beta = 1/2;
     
-    delta_error = sech(beta * (weights_out * input - biase))^2 * beta * input * (targets - predicted);
+    g_prime = sech(beta * (weights_out * input.' - biase))^2 * beta;
+    delta_error =(targets - predicted) * g_prime;
     
-    W = (learning_rate * weights_out.' .* delta_error).';
+    W = learning_rate * delta_error .* input;
+end
+
+function b = update_biase_out(targets, predicted, weights_out, input, biase)
+    learning_rate = 0.02;
+    beta = 1/2;
+    
+    b = learning_rate*((targets - predicted))*(-sech(beta * (weights_out * input.' - biase))^2 * beta);
 end
 
 function g = activation(b)
