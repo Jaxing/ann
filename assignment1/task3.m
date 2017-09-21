c37b6803ec321664a2db958936243a9b4168f8b0
Jesper Jaxing <jaxing@student.chalmers.se>
2017-09-21 12:02:23 +0200

    add labels to plot

--- assignment1/task3.m
+++ assignment1/task3.m
@@ -31,13 +31,13 @@ beta = 1/2;
 weights = rand(2,1)* 0.4 -0.2;
 biase = rand * 2 - 1;
 
-energy_train = zeros(1,10^6);
-energy_validation = zeros(1,10^6);
+energy_train = zeros(1,10^3);
+energy_validation = zeros(1,10^3);
 iteration_range = 1:10^6;
 
 train_error = class_error(training_target, training_input, weights, biase)
 val_error = class_error(validation_target, validation_input, weights, biase)
-
+l = 1;
 for iteration = iteration_range
     tic
     i = mod(iteration, length(training_target));
@@ -45,11 +45,15 @@ for iteration = iteration_range
         i = length(training_target);
     end
     prediction = predict(training_input(i,:), weights, biase);
-    energy_train(iteration) = energy_function(training_target, training_input, weights, biase);
-    energy_validation(iteration) = energy_function(validation_target, validation_input, weights, biase);
-    weights(1,1) = weights(1,1) + update_weights(1,1,training_target(i), prediction, weights, training_input(i,:), biase);
-    weights(2,1) = weights(2,1) + update_weights(1,2,training_target(i), prediction, weights, training_input(i,:), biase);
-    biase = biase + update_biase(1,1,training_target(i), prediction, weights, training_input(i,:), biase);
+    if mod(iteration, 1000) == 0
+        energy_train(l) = energy_function(training_target, training_input, weights, biase);
+        energy_validation(l) = energy_function(validation_target, validation_input, weights, biase);
+        l = l + 1;
+    end
+    weights = weights + update_all_weights(training_target(i), prediction, weights, training_input(i,:), biase);
+    %weights(1,1) = weights(1,1) + update_weights(1,1,training_target(i), prediction, weights, training_input(i,:), biase);
+    %weights(2,1) = weights(2,1) + update_weights(1,2,training_target(i), prediction, weights, training_input(i,:), biase);
+    biase = biase + update_biase(training_target(i), prediction, weights, training_input(i,:), biase);
     one_it = toc;
 end
 train_error = class_error(training_target, training_input, weights, biase)
@@ -57,8 +61,10 @@ val_error = class_error(validation_target, validation_input, weights, biase)
 
 
 hold on
-plot(iteration_range, energy_train);
-plot(iteration_range, energy_validation);
+plot(1:10^3, energy_train);
+plot(1:10^3, energy_validation);
+xlabel('iterations')
+ylabel('energy')
 
 function c = class_error(targets, inputs, weights, biase)
     p = length(targets);
@@ -73,13 +79,8 @@ end
 
 function f = predict(input, weights, biase)
     beta = 1/2;
-    s=0;
     
-    for i=1:length(input)
-        s = s + tanh(beta*(input(i)*weights(i)-biase));
-    end
-    
-    f = s;
+    f = tanh(beta*(input * weights - biase));
 end
 
 function H = energy_function(target, input, weights, biase)
@@ -92,18 +93,28 @@ function H = energy_function(target, input, weights, biase)
     H = summa/2;
 end
 
-function b = update_biase(output_index, input_index, target, predicted, weights, input, biase)
+function b = update_biase(target, predicted, weights, input, biase)
     learning_rate = 0.02;
     beta = 1/2;
     
-    b = learning_rate*((target - predicted))*sech(beta*input*weights - biase)^2 * beta;
+    b = learning_rate*((target - predicted))*(-sech(beta*input*weights - biase)^2 * beta);
 end
 
 function W = update_weights(output_index, input_index, target, predicted, weights, input, biase)
     learning_rate = 0.02;
     beta = 1/2;
     
-    W = learning_rate*((target - predicted))*sech(beta*input*weights - biase)^2 * beta * input(:, input_index) * input(:, input_index);
+    W = learning_rate*((target - predicted))* sech(beta*input*weights)^2 * beta * input(:, input_index);
 
     %W =(target(output_index)-predicted(output_index))*weights(input_index, output_index)*input(:,input_index);
+end
+
+function W = update_all_weights(target, predicted, weights, input, biase)
+    learning_rate = 0.02;
+    beta = 1/2;
+    
+    g_prime = sech(beta * input * weights - biase).^2 * beta;
+    delta_error = (target - predicted) * g_prime;
+    
+    W = learning_rate * delta_error .* input.';
 end
